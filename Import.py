e2ee1b2635fc20863cd100f290ead1ed2ce3f585
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-05-09 13:24:37 -0700

    Update Import.py

--- Import.py
+++ Import.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 import sys
 import xml.etree.ElementTree as ET
@@ -28,8 +29,51 @@ import xml.etree.ElementTree as ET
 if len(sys.argv) < 2:
     print("You must pass at least one XML file name containing Pokemon to insert")
 
+
+# connect to database
+conn = sqlite3.connect('pokemon.sqlite')
+c = conn.cursor()
+
+
 for i, arg in enumerate(sys.argv):
     # Skip if this is the Python filename (argv[0])
     if i == 0:
         continue
 
+    # prase xml file
+    tree = ET.parse(arg)
+    root = tree.getroot()
+    
+    # extract pokemon data
+    pokedex = root.attrib.get('pokedex', None)
+    classification = root.attrib.get('classification', None)
+    generation = root.attrib.get('generation', None)
+    name = root.attrib.get('name', None)
+    hp = root.attrib.get('hp', None)
+    type = [type.text for type in root.findall('type') ]
+    attack = root.attrib.get('attack', None)
+    defense = root.attrib.get('defense', None)
+    speed = root.attrib.get('speed', None)
+    sp_attack = root.attrib.get('sp_attack', None)
+    sp_defense = root.attrib.get('sp_defense', None)
+    height = root.attrib.get('height', None)
+    weight = root.attrib.get('weight', None)
+    abilities = [abilities.text for abilities in root.findall('abilities/ability')]
+
+    # check if pokemon already exist in the database
+    c.execute("SELECT COUNT(*) FROM pokemon WHERE name=?", (name,))
+    count = c.fetchone()[0]
+    if count > 0:
+        continue
+    
+    # insert new pokemon into database
+    c.execute("INSERT INTO pokemon(pokedex, classification, generation, hp, type, attack, defense, speed, sp_attack, sp_defense, height, weight, abilities) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)", (pokedex, classification, generation, hp, '|'.join(type), attack, defense, speed, sp_attack, sp_defense, height, weight, abilities))
+    pokemon_id = c.lastrowid
+    
+    # insert abilities into database
+    for ability in abilities:
+        c.execute("INSERT INTO pokemon_abilities(pokemon_id, ability) values(?,?)", (pokemon_id, ability))
+
+# commit change and close the connection
+conn.commit()
+conn.close()
